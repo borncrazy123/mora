38bdf44ec44c327e3f3d8bee965226450e9c5a38
chenchao <borncrazy123@163.com>
2013-09-16 21:25:17 +0800

    add listen.

--- gen/com/example/mora/R.java
+++ gen/com/example/mora/R.java
@@ -22,13 +22,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int image=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int mail=0x7f020003;
+        public static final int map=0x7f020004;
+        public static final int time=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080003;
-        public static final int editText1=0x7f080002;
-        public static final int textView2=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int imageButton1=0x7f080004;
+        public static final int imageButton2=0x7f080005;
+        public static final int imageButton20=0x7f080000;
+        public static final int imageButton3=0x7f080006;
+        public static final int imageButton4=0x7f080007;
+        public static final int textView1=0x7f080001;
+        public static final int toggleButton1=0x7f080002;
+        public static final int toggleButton2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,6 +49,8 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050003;
+        public static final int textview=0x7f050005;
+        public static final int togglebutton=0x7f050004;
         public static final int win_title_name=0x7f050001;
     }
     public static final class style {
--- src/com/example/mora/MainActivity.java
+++ src/com/example/mora/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.mora;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+	private ToggleButton toggleButton1 = null;
+	private ToggleButton toggleButton2 = null;
+	private ImageButton imageButton1 = null;
+	private ImageButton imageButton2 = null;
+	private ImageButton imageButton3 = null;
+	private ImageButton imageButton4 = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-//		sysout
+
+		toggleButton1 = (ToggleButton) this.findViewById(R.id.toggleButton1);
+		toggleButton1.setChecked(false);
+		toggleButton1.setEnabled(false);
+		toggleButton2 = (ToggleButton) this.findViewById(R.id.toggleButton2);
+		toggleButton2.setChecked(false);
+		toggleButton2.setEnabled(false);
+
+		imageButton1 = (ImageButton) this.findViewById(R.id.imageButton1);
+		imageButton1.setOnClickListener(this);
+		imageButton2 = (ImageButton) this.findViewById(R.id.imageButton2);
+		imageButton2.setOnClickListener(this);
+		imageButton3 = (ImageButton) this.findViewById(R.id.imageButton3);
+		imageButton3.setOnClickListener(this);
+		imageButton4 = (ImageButton) this.findViewById(R.id.imageButton4);
+		imageButton4.setOnClickListener(this);
 	}
 
 	@Override
@@ -25,14 +47,59 @@ public class MainActivity extends Activity {
 	}
 
 	public void sendMessage(View view) {
-		Intent intent = new Intent(this, MainActivity.class);
-		EditText editText = (EditText) findViewById(R.id.editText1);
-		String message = editText.getText().toString();
-		System.out.println("input message:" + message);
-		intent.putExtra("xxxx", message);
+		// Intent intent = new Intent(this, MainActivity.class);
+		// EditText editText = (EditText) findViewById(R.id.editText1);
+		// String message = editText.getText().toString();
+		// System.out.println("input message:" + message);
+		// intent.putExtra("xxxx", message);
+		//
+		// TextView textView = (TextView) findViewById(R.id.textView2);
+		// textView.setText("You send message:" + message);
+	}
 
-		TextView textView = (TextView) findViewById(R.id.textView2);
-		textView.setText("You send message:" + message);
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.imageButton1: {
+			setToggleButtonState(true);
+			break;
+		}
+		case R.id.imageButton2: {
+			setToggleButtonState(true);
+			break;
+		}
+		case R.id.imageButton3: {
+			setToggleButtonState(true);
+			break;
+		}
+		case R.id.imageButton4: {
+			setToggleButtonState(false);
+			break;
+		}
+		}
+
+	}
+
+	private void setToggleButtonState(boolean isChecked) {
+		if (isChecked) {
+			if (toggleButton1.isChecked()) {
+				toggleButton2.setChecked(isChecked);
+
+				setImageButtonState(false);
+			} else {
+				toggleButton1.setChecked(isChecked);
+			}
+		} else {
+			toggleButton1.setChecked(isChecked);
+			toggleButton2.setChecked(isChecked);
+			setImageButtonState(true);
+		}
+	}
+
+	private void setImageButtonState(boolean isEnable) {
+		imageButton1.setEnabled(isEnable);
+		imageButton2.setEnabled(isEnable);
+		imageButton3.setEnabled(isEnable);
 	}
 
 }
